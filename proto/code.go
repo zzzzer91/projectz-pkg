104dac52d02861775933e8c800f07cb0cdeae8a9
zzzzer <zzzzer91@gmail.com>
2023-05-14 19:23:28 +0800

    feat: add new code

---
+++ proto/code.go
@@ -0,0 +1,15 @@
+package proto
+
+const (
+	SuccessCodeDefault      = 0
+	ErrorCodeClientDefault  = 40000
+	ErrorCodeClientNotFound = 40400
+	ErrorCodeServerDefault  = 50000
+)
+
+const (
+	SuccessMsgDefault      = "OK"
+	ErrorMsgClientDefault  = "Bad Request"
+	ErrorMsgClientNotFound = "Not Found"
+	ErrorMsgServerDefault  = "Internal Server Error"
+)
